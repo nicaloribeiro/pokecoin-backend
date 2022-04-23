2bdab70f6763281a64641ca9971ead143863e048
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-23 18:51:51 -0300

    Adicao das rotas de aquisicao de pokemon na api

--- src/config/express.js
+++ src/config/express.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const PokemonAcquiredRouter = require('../routes/PokemonAcquiredRoutes');
+
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/api/buy', PokemonAcquiredRouter);
 
 module.exports = app;
