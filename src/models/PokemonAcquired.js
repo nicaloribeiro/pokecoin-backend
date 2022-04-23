475ed1bc9cd6a76084d4df699e9e038d723b1126
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-23 18:49:57 -0300

    Criacao da model do pokemon adquirido

---
+++ src/models/PokemonAcquired.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const pokemonAcquiredSchema = new Schema({
+    pokemonId: { type: String, required: true },
+    pokemonName: { type: String, required: true },
+    pokemonExperience: { type: Number, required: true },
+    pokemonSpriteUrl: { type: String, required: true },
+    inWallet: { type: Boolean, required: true}
+});
+
+const PokemonAcquired = mongoose.model('PokemonAcquired', pokemonAcquiredSchema);
+
+module.exports = PokemonAcquired;
