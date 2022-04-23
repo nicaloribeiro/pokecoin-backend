3b779a807f945a01039628ac8fae8d2ac446077e
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-23 20:39:32 -0300

    Criacao da rota para venda de pokemons

--- src/routes/PokemonAcquiredRoutes.js
+++ src/routes/PokemonAcquiredRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const pokemonAcquiredRoutes = express.Router();
 const pokemonAcquiredControllers = require('../controller/PokemonAcquiredController');
-const { postPokemonAcquired, getAllPokemonAcquired } = pokemonAcquiredControllers;
+const { postPokemonAcquired, getAllPokemonAcquired, postPokemonAcquiredSale } = pokemonAcquiredControllers;
 
-pokemonAcquiredRoutes.post('/pokemon/new', postPokemonAcquired);
+pokemonAcquiredRoutes.post('/pokemon/buy', postPokemonAcquired);
 pokemonAcquiredRoutes.get('/pokemon/all', getAllPokemonAcquired);
+pokemonAcquiredRoutes.post('/pokemon/sell/:pokemonAcquiredId', postPokemonAcquiredSale);
 
 module.exports = pokemonAcquiredRoutes;
