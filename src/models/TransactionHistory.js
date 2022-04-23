52160d6ec9d5090908d249e6882a2f9ce0ab39a7
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-23 19:59:19 -0300

    Criacao da model para historico de compras

---
+++ src/models/TransactionHistory.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const transactionTypeEnum = {
+    buy: 'BUY',
+    sale: 'SALE'
+}
+
+const transactionHistorySchema = new Schema({
+    btcCurrency: { type: String, required: true },
+    transactionType : { 
+        type: String,
+        enum: Object.values(transactionTypeEnum),
+        required: true
+    },
+    transactionDate : { type: Date, default: Date.now },
+    pokemonId: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'PokemonAcquired',
+        required: true
+    }
+});
+
+const TransactionHistory = mongoose.model('TransactionHistory', transactionHistorySchema);
+
+module.exports = TransactionHistory;
