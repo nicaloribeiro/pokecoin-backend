541eba4a776da51fccf5dd6894888f219c9f7703
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-23 17:25:25 -0300

    Realizando conexao com o mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const app = require('./config/express');
 const port = app.get('port');
 
+const database = require('./config/mongodb');
+
+try {
+    database.mongoose.connect(database.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, () => {
+      console.log('Conectado ao MongoDB!');
+  });
+} catch (error) {
+    console.log('Houve um problema ao conectar ao mongodb.')
+    console.log(error)
+};
+
+const dbhandler = database.mongoose.connection
+dbhandler.on('error', console.error.bind(console, 'Erro na conexão com o MongoDB:'));
+
 app.get('/api/helloworld', function(req, res) {
     res.status(200).json({data: 'hello world'});
 });
