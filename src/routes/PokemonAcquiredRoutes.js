d3e943f29196923f9237663af9dd205f2c6e4758
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-23 18:59:23 -0300

    Rota para listagem de pokemons

--- src/routes/PokemonAcquiredRoutes.js
+++ src/routes/PokemonAcquiredRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const pokemonAcquiredRoutes = express.Router();
 const pokemonAcquiredControllers = require('../controller/PokemonAcquiredController');
-const { postPokemonAcquired } = pokemonAcquiredControllers;
+const { postPokemonAcquired, getAllPokemonAcquired } = pokemonAcquiredControllers;
 
 pokemonAcquiredRoutes.post('/pokemon/new', postPokemonAcquired);
+pokemonAcquiredRoutes.get('/pokemon/all', getAllPokemonAcquired);
 
 module.exports = pokemonAcquiredRoutes;
